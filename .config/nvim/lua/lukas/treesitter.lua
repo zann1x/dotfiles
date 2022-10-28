7768e047444ac50db1339145f79970ee02ba18e6
Lukas Zanner <git@lukaszanner.de>
2022-10-28 23:16:42 +0200

    Set treesitter's auto_install to false

--- .config/nvim/lua/lukas/treesitter.lua
+++ .config/nvim/lua/lukas/treesitter.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
     ensure_installed = { "c", "cmake", "cpp", "go", "gomod", "rust" },
     sync_install = false,
-    auto_install = true,
+    auto_install = false,
 
     highlight = {
         enable = true,
