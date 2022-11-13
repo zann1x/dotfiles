84503512cd5ed04fa5ce8c9eb1e1969ebe6effb7
Lukas Zanner <git@lukaszanner.de>
2022-11-13 21:04:52 +0100

    Properly load the telescope-fzf-native extension

--- .config/nvim/lua/lukas/telescope.lua
+++ .config/nvim/lua/lukas/telescope.lua
@@ -3,3 +3,6 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+
+-- Load up the telescope-fzf-native.nvim extension
+require('telescope').load_extension('fzf')
