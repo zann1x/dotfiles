c5dfa1e2b1d45f0806def73a04ef4757743b41d4
Lukas Zanner <git@lukaszanner.de>
2023-04-01 23:50:49 +0200

    Add remap.lua for specific neovim keybindings

---
+++ .config/nvim/lua/lukas/remap.lua
@@ -0,0 +1,4 @@
+-- Most of the remaps are in the .vimrc to be accessible in normal terminals
+-- as well. The remaps here are more or less specific to neovim.
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
