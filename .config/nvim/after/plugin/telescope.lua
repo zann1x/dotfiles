eef54655ea3ecbca468c690041ada21af7be8aae
Lukas Zanner <git@lukaszanner.de>
2023-04-01 19:39:45 +0200

    Update key mappings for telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,8 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 
 -- Load up the telescope-fzf-native.nvim extension
 require('telescope').load_extension('fzf')
