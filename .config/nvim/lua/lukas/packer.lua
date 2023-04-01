76698000bf17b72c2ae0ba3c08edd1f0e898ef69
Lukas Zanner <git@lukaszanner.de>
2023-04-02 00:03:55 +0200

    Add nvim plugin cellular-automaton

--- .config/nvim/lua/lukas/packer.lua
+++ .config/nvim/lua/lukas/packer.lua
@@ -34,6 +34,9 @@ return require('packer').startup(function(use)
     end
   }
 
+  -- When shit hits the fan
+  use 'eandrju/cellular-automaton.nvim' 
+
   -- Git integration
   use 'tpope/vim-fugitive'
 
--- .config/nvim/lua/lukas/remap.lua
+++ .config/nvim/lua/lukas/remap.lua
@@ -2,3 +2,4 @@
 -- as well. The remaps here are more or less specific to neovim.
 
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>fml', "<cmd>CellularAutomaton make_it_rain<CR>")
