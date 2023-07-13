39a08544cfcdd3973c35a0851f3a175b7715f513
Lukas Zanner <git@lukaszanner.de>
2023-07-13 19:59:00 +0200

    Bump telescope.nvim from 0.1.0 to 0.1.2

--- dot_config/nvim/lua/lukas/packer.lua
+++ dot_config/nvim/lua/lukas/packer.lua
@@ -49,7 +49,7 @@ return require('packer').startup(function(use)
 
   -- Fuzzy finder
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    'nvim-telescope/telescope.nvim', tag = '0.1.2',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
   -- Telescope sorter to improve sorting performance
