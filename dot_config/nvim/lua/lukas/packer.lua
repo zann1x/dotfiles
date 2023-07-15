aa7e06e2c98b9ff8252ab452025ae40c9174e9cc
Lukas Zanner <git@lukaszanner.de>
2023-07-15 20:22:50 +0200

    Add nvim-autopairs

--- dot_config/nvim/lua/lukas/packer.lua
+++ dot_config/nvim/lua/lukas/packer.lua
@@ -109,6 +109,21 @@ return require('packer').startup(function(use)
       end
   }
 
+  -- Automatically finish pairs of... things
+  use {
+    'windwp/nvim-autopairs',
+    requires = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require('nvim-autopairs').setup {}
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    end
+  }
+
   -- Convenient commenting of lines
   use {
     'numToStr/Comment.nvim',
