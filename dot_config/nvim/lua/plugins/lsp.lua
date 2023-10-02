e4d0d8b2c9309b523bdff62b8f8cd6dc629172c2
Lukas Zanner <git@lukaszanner.de>
2023-10-02 15:33:29 +0200

    Replace none-ls with conform

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,22 @@
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
+
 return {
     -- Autocompletion
     {
@@ -164,89 +183,29 @@ return {
                     },
                 },
             })
-
-            -- Switch for controlling whether you want autoformatting.
-            --  Use :LukasFormatToggle to toggle autoformatting on or off
-            local format_is_enabled = true
-            vim.api.nvim_create_user_command("LukasFormatToggle", function()
-                format_is_enabled = not format_is_enabled
-                print("Setting autoformatting to: " .. tostring(format_is_enabled))
-            end, {})
-
-            -- Create an augroup that is used for managing our formatting autocmds.
-            --      We need one augroup per client to make sure that multiple clients
-            --      can attach to the same buffer without interfering with each other.
-            local _augroups = {}
-            local get_augroup = function(client)
-                if not _augroups[client.id] then
-                    local group_name = "lukas-lsp-format-" .. client.name
-                    local id = vim.api.nvim_create_augroup(group_name, { clear = true })
-                    _augroups[client.id] = id
-                end
-
-                return _augroups[client.id]
-            end
-
-            -- Whenever an LSP attaches to a buffer, we will run this function.
-            --
-            -- See `:help LspAttach` for more information about this autocmd event.
-            vim.api.nvim_create_autocmd("LspAttach", {
-                group = vim.api.nvim_create_augroup("lukas-lsp-attach-format", { clear = true }),
-                -- This is where we attach the autoformatting for reasonable clients
-                callback = function(args)
-                    local client_id = args.data.client_id
-                    local client = vim.lsp.get_client_by_id(client_id)
-                    local bufnr = args.buf
-
-                    -- Only attach to clients that support document formatting
-                    if not client.server_capabilities.documentFormattingProvider then
-                        return
-                    end
-
-                    -- Tsserver usually works poorly. Sorry you work with bad languages
-                    -- You can remove this line if you know what you're doing :)
-                    if client.name == "tsserver" then
-                        return
-                    end
-
-                    -- Create an autocmd that will run *before* we save the buffer.
-                    --  Run the formatting command for the LSP that has just attached.
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = get_augroup(client),
-                        buffer = bufnr,
-                        callback = function()
-                            if not format_is_enabled then
-                                return
-                            end
-
-                            vim.lsp.buf.format {
-                                async = false,
-                                filter = function(c)
-                                    return c.id == client.id
-                                end,
-                            }
-                        end,
-                    })
-                end,
-            })
         end,
     },
 
     -- Bridge the gap between LSP and formatter, linters and alike
     {
-        "nvimtools/none-ls.nvim",
+        "stevearc/conform.nvim",
+        cmd = "ConformInfo",
         event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "nvim-lua/plenary.nvim",
+        opts = {
+            formatters_by_ft = {
+                cpp = { "clang-format" },
+                go = { "goimports", "golines", "gofmt" },
+                rust = { "rustfmt" },
+            },
+            format_on_save = function(bufnr)
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                return {
+                    lsp_fallback = true,
+                    timeout_ms = 500,
+                }
+            end,
         },
-        opts = function()
-            local nls = require("null-ls")
-            return {
-                sources = {
-                    nls.builtins.formatting.goimports,
-                    nls.builtins.formatting.golines,
-                },
-            }
-        end,
     },
 }
