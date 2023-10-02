baf24d822000f36167b73fe70fd466c3f62d3eb2
Lukas Zanner <git@lukaszanner.de>
2023-10-02 15:43:48 +0200

    Replace vim-fugitive with neogit

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,9 +1,16 @@
 return {
     -- Git integration
     {
-        "tpope/vim-fugitive",
-        keys = {
-            { "<leader>gs", vim.cmd.Git, desc = "[G]it [S]tatus" },
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+            "sindrets/diffview.nvim",
+            "ibhagwan/fzf-lua",
         },
+        keys = {
+            { "<leader>gs", function() require("neogit").open() end, desc = "[G]it [S]tatus" },
+        },
+        config = true,
     },
 }
