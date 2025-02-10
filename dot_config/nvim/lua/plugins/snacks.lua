ae91e8b5d5001cbbef5fc8a506cb5dcbf3322ae9
Lukas Zanner <git@lukaszanner.de>
2025-02-10 09:33:07 +0100

    Add snacks.vim configuration

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+        bigfile = { enabled = true },
+        indent = { enabled = true },
+        quickfile = { enabled = true },
+        words = { enabled = true },
+    },
+}
